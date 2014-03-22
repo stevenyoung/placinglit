30ce7c548d1f629f3dc8acd665151e011b928793
Steven Young <fivepockets@gmail.com>
2014-03-22 16:10:18 -0400

    simple model for scene collections

--- classes/collections.py
+++ classes/collections.py
@@ -4,24 +4,12 @@
 from google.appengine.ext import db
 
 
-@db.transactional(xg=True)
-def add_scene(place_key):
-  """ add scene """
-  collection = Collection.get_or_insert(key_name='QLD')
-  collection.scenes.append(place_key)
-  collection.put()
-  name = collection.key().name()
-  print collection.key().name()
-  assert name == "QLD"
-
-
 class Collection(db.Model):
   """ Collections of scenes """
-
   name = db.StringProperty()
   url = db.LinkProperty()
   scenes = db.ListProperty(db.Key)
 
-  def __init__(self, key_name=None):
-    self.key_name = key_name
-    super(Collection, self).__init__()
+  def add_scene(self, scene_key):
+    self.scenes.append(scene_key)
+    self.put()
