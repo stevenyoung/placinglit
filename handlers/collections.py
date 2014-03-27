563f1cd054871f56a8bcfea1e4ea42b5fd199823
Steven Young <fivepockets@gmail.com>
2014-03-26 21:24:04 -0400

    render map for collected scenes

--- handlers/collections.py
+++ handlers/collections.py
@@ -1,8 +1,8 @@
+""" Handle web requests for collections """
+# pylint disable=C0103
 import json
-import logging
 
 from google.appengine.ext import webapp
-from google.appengine.ext import users
 
 from handlers.abstracts import baseapp
 
@@ -12,14 +12,22 @@ from classes import placedlit
 
 class SceneCollectionHandler(baseapp.BaseAppHandler):
   """ Web handler for scene collections """
-  def get(self, collection_name):
-    scene_keys = collections.Collection().get_named(collection_name)
-    scene_json = dict()
+  def get(self, collection_name):  # pylint disable=W0221
+    """ get request for collection """
+    template_values = self.basic_template_content()
+    template_values['title'] = 'Map'
+    scene_keys = collections.Collection().get_named(collection_name).scenes
+    if 'center' in collections.FEATURED[collection_name]:
+      collection_center = collections.FEATURED[collection_name]['center']
+      template_values['center'] = '{lat:%s,lng:%s}' % (collection_center['lat'],
+                                                       collection_center['lng'])
+    scenes_json = list()
     for key in scene_keys:
-      place = placedlit.PlacedLit().get_by_key_name(key)
-      logging.info('place:', key, place)
-      scene_json.append({'title': place.title})
+      scenes_json.append(self.export_place_fields(
+                        placedlit.PlacedLit().get(key)))
+    template_values['scenes'] = json.dumps(scenes_json)
+    self.render_template('map.tmpl', template_values)
 
-urls = [('/collections/(.*)', SceneCollectionHandler)]
+handler_urls = [('/collections/(.*)', SceneCollectionHandler)]
 
-app = webapp.WSGIApplication(urls, debug=True)
+app = webapp.WSGIApplication(handler_urls, debug=True)
