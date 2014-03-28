3222178857352fc8b56e5b5922948883620883c1
Steven Young <fivepockets@gmail.com>
2014-03-28 00:23:39 -0400

    update user isbns for matching title

--- handlers/update_scenes.py
+++ handlers/update_scenes.py
@@ -1,3 +1,4 @@
+from google.appengine.ext import db
 from google.appengine.ext import deferred
 from google.appengine.ext import webapp
 
@@ -12,8 +13,29 @@ class UpdateBooksHandler(webapp.RequestHandler):
 
 class UpdateUserISBNHandler(webapp.RequestHandler):
   def get(self):
-    deferred.defer(update_schema.update_user_isbns)
-    self.response.out.write('Schema migration successfully initiated.')
+    # deferred.defer(update_schema.update_user_isbns)
+    self.response.out.write('isbn update successfully initiated.<br/>')
+    place_query = db.GqlQuery('SELECT * FROM PlacedLit WHERE ug_isbn != :1',
+                              None)
+    title_isbns = dict()
+    to_put = list()
+    for place in place_query:
+      title_isbns[place.title] = place.ug_isbn
+
+    # print title_isbns
+    # print len(title_isbns)
+    for title, isbn in title_isbns.iteritems():
+      # self.response.out.write('{}:{}<br/>'.format(title, isbn))
+      # print isbn
+      title_query = db.GqlQuery(
+        'SELECT * from PlacedLit WHERE title = :1 AND ug_isbn = :2',
+        title, None)
+      for scene in title_query:
+        scene.ug_isbn = isbn
+        to_put.append(scene)
+        self.response.out.write('updating {} ({})</br>'.format(title, isbn))
+    db.put(to_put)
+    self.response.out.write(' done.')
 
 
 app = webapp.WSGIApplication([
