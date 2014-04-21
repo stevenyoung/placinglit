04ea533e6e9d31653181feb1300f10960f569792
Steven Young <fivepockets@gmail.com>
2014-04-21 14:39:06 -0400

    update site_users user deferred api

--- handlers/update_schema.py
+++ handlers/update_schema.py
@@ -4,9 +4,11 @@ import logging
 
 from google.appengine.ext import deferred
 from google.appengine.ext import db
+from google.appengine.ext import ndb
 
 from classes import placedlit
 from classes import books
+from classes import site_users
 # from classes import authors
 
 BATCH_SIZE = 100  # ideal batch size may vary based on entity size.
@@ -21,6 +23,34 @@ def transform_author_name_for_query(name):
   else:
     query_name = name.lower()
   return query_name
+
+
+def update_user_scene_data(cursor=None, num_updated=0):
+  """ add scenes to site users """
+  scene_query = placedlit.PlacedLit.all().filter('user_email !=', NULL)
+  if cursor:
+    scene_query.with_cursor(cursor)
+
+  users_to_put = list()
+  for scene in scene_query.fetch(limit=BATCH_SIZE):
+    logging.debug('email %s', scene.user_email)
+    if scene.user_email:
+      user = site_users.User.get_by_id(scene.user_email)
+      if not user:
+        user = site_users.User.create(scene.user_email)
+      user.email = scene.user_email
+      user.added_scenes.append(ndb.Key.from_old_key(scene.key()))
+      users_to_put.append(user)
+
+  if users_to_put:
+    ndb.put_multi(users_to_put)
+    num_updated += len(users_to_put)
+    logging.debug('Put %d entities to Datastore for a total of %d',
+                  len(users_to_put), num_updated)
+    deferred.defer(update_user_scene_data, cursor=scene_query.cursor(),
+                   num_updated=num_updated)
+  else:
+    logging.debug('Update user scenes complete with %d updates!', num_updated)
 
 
 def update_book_data(cursor=None, num_updated=0):
