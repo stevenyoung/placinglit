d46d6ba9ee2ac5839cfbc70fcd800e76a629ad51
Steven Young <fivepockets@gmail.com>
2014-05-18 17:24:18 -0400

    moved new scene handler

--- handlers/places.py
+++ handlers/places.py
@@ -3,7 +3,6 @@
 
 
 import datetime
-import json
 import logging
 
 from string import capwords
@@ -14,69 +13,11 @@ from google.appengine.api import memcache
 
 from handlers.abstracts import baseapp
 from classes import placedlit
-from classes import user_request
-from classes import site_users
 
 
 def is_image_url(url):
+  """ supported image url? """
   return url.rsplit('.')[-1] in ['jpg', 'png', 'gif']
-
-
-class AddPlacesHandler(baseapp.BaseAppHandler):
-  """ adding a place from user interaction """
-  def post(self):
-    """ add scene from user submission """
-    self.place_data = json.loads(self.request.body)
-    self.place_data['user'] = users.get_current_user()
-    self.place_data['email'] = users.get_current_user().email()
-    place_key = placedlit.PlacedLit.create_from_dict(self.place_data)
-    self.add_scene_to_user(scene_key=place_key)
-    agent = self.request.headers['User-Agent']
-    user_request.UserRequest.create(ua=agent, user_loc=place_key)
-    self.send_response()
-    self.post_place_to_twitter(scene_key=place_key)
-
-  def add_scene_to_user(self, scene_key=None):
-    """ update a users added and vistited scenes """
-    user_email = self.place_data['email']
-    if user_email:
-      user = site_users.User.get_by_id(user_email)
-      if not user:
-        user = site_users.User.create(user_email)
-      user.add_scene(scene_key)
-      if self.place_data['check_in']:
-        user.visit_scene(scene_key)
-
-  def send_response(self):
-    """ format user client response """
-    scene_data = self.place_data
-    response = '{} by {} added at <br>location: ({}, {})<br>thanks.'
-    response_message = response.format(
-      scene_data['title'], scene_data['author'],
-      scene_data['latitude'], scene_data['longitude']
-    )
-    response_json = {
-      'message': response_message,
-      'geopt': {'lat': scene_data['latitude'], 'lng': scene_data['longitude']}
-    }
-    self.output_json(response_json)
-
-  def post_place_to_twitter(self, scene_key=None):
-    """ update twitter status """
-    import os
-    # do not post to twitter if running on dev
-    if not os.environ['SERVER_SOFTWARE'].startswith('Dev'):
-      scene_data = placedlit.PlacedLit.get_place_from_id(scene_key.id())
-      from handlers import twitter
-      from handlers.abstracts import keys
-      oauth = twitter.OAuth(token=keys.tw_keys['OAUTH_TOKEN'],
-                            token_secret=keys.tw_keys['OAUTH_TOKEN_SECRET'],
-                            consumer_key=keys.tw_keys['CONSUMER_KEY'],
-                            consumer_secret=keys.tw_keys['CONSUMER_SECRET'])
-      t = twitter.Twitter(auth=oauth)
-      status = "{} by {} was mapped on PlacingLiterature.com. #literaryroadtrip"
-      update = status.format(scene_data.title, scene_data.author)
-      t.statuses.update(status=update)
 
 
 class GetPlacesHandler(baseapp.BaseAppHandler):
@@ -225,7 +166,6 @@ class PlacesTitles(baseapp.BaseAppHandler):
 
 
 urls = [
-  ('/places/add', AddPlacesHandler),
   ('/places/show', GetPlacesHandler),
   ('/places/info/(.*)', InfoHandler),
   ('/places/visit/(.*)', PlacesVisitHandler),
