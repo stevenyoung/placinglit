823ad8b6dbb38087aa19028c98bdef258732e509
Steven Young <fivepockets@gmail.com>
2014-05-20 16:02:27 -0400

    consider removing handler for updating photos for all scenes

--- handlers/panoramio.py
+++ handlers/panoramio.py
@@ -1,15 +1,11 @@
 """ request handler for panaramio photos """
 # pylint: disable=W0403, R0904, C0103
 
-import logging
 
-from google.appengine.ext import db
 from google.appengine.ext import deferred
 from google.appengine.ext import webapp
 
-from handlers.abstracts import baseapp
 
-from classes import panoramio
 from classes import photo_index
 
 import update_schema
@@ -21,26 +17,27 @@ class UpdatePhotosBatchHandler(webapp.RequestHandler):
     self.response.out.write('Schema migration successfully initiated.')
 
 
-class UpdateAllPhotosHandler(baseapp.BaseAppHandler):
-  """ get panaramio photos for scenes without photos """
-  def get(self):
-    scene_query = db.GqlQuery('SELECT __key__ from PlacedLit')
-    scene_count = 0
-    photo_count = 0
-    for key in scene_query.run():
-      if photo_index.has_panoramio_photos(key.id()):
-        logging.info('%s already has photos', key.id())
-      else:
-        url = panoramio.build_url_for_scene(scene_key=key)
-        api_response = panoramio.get_api_data(url)
-        if 'map_location' in api_response:
-          map_location = api_response['map_location']
-          for photo in api_response['photos']:
-            photo_count += 1
-            panoramio.Panoramio.save_images_for_scene(scene_key=key, data=photo,
-                                                      location=map_location)
-      scene_count += 1
-    logging.info('added %s pix for %s scenes', photo_count, scene_count)
+#FIXIT - Is this necessary any longer?
+# class UpdateAllPhotosHandler(baseapp.BaseAppHandler):
+#   """ get panaramio photos for scenes without photos """
+#   def get(self):
+#     scene_query = db.GqlQuery('SELECT __key__ from PlacedLit')
+#     scene_count = 0
+#     photo_count = 0
+#     for key in scene_query.run():
+#       if photo_index.has_panoramio_photos(key.id()):
+#         logging.info('%s already has photos', key.id())
+#       else:
+#         url = panoramio.build_url_for_scene(scene_key=key)
+#         api_response = panoramio.get_api_data(url)
+#         if 'map_location' in api_response:
+#           map_location = api_response['map_location']
+#           for photo in api_response['photos']:
+#             photo_count += 1
+#             panoramio.Panoramio.save_images_for_scene(scene_key=key, data=photo,
+#                                                       location=map_location)
+#       scene_count += 1
+#     logging.info('added %s pix for %s scenes', photo_count, scene_count)
 
 
 class EmptyPhotoIndexHandler(webapp.RequestHandler):
