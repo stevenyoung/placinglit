c43d98f92e85b700daa17afc278994c94ef5b78a
Steven Young <fivepockets@gmail.com>
2014-05-29 21:50:53 -0400

    unique authors and titles for form suggestions

--- classes/status_updates.py
+++ classes/status_updates.py
@@ -16,7 +16,7 @@ class StatusUpdate(ndb.Model):
     self.status_text = status_text
     self.put()
 
-  def is_duplicate_update(self, status_text=status_text):
+  def is_duplicate_update(self, status_text=None):
     update_query = StatusUpdate.query(StatusUpdate.status_text == status_text)
     matching_updates = [result for result in update_query.iter()]
     if len(matching_updates) < 1:
