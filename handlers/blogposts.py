8f96fddd466935fc62bed9c67f26118a038950af
Steven Young <fivepockets@gmail.com>
2014-06-20 12:10:19 -0400

    make pylint happy

--- handlers/blogposts.py
+++ handlers/blogposts.py
@@ -1,3 +1,8 @@
+"""
+Retrieve blog posts from Wordpress RSS.
+"""
+# pylint: disable=R0904, R0914, C0103
+
 import logging
 
 import xml.etree.ElementTree as et
@@ -14,7 +19,9 @@ BLOG_FEED_URL = 'http://placingliterature.wordpress.com/feed/'
 
 
 class BlogpostsHandler(baseapp.BaseAppHandler):
+  """ request handler for blog post admin """
   def get(self):
+    """ update blogposts for home page """
     fetch_headers = {'Cache-Control': 'no-cache,max-age=300',
                      'Pragma': 'no-cache'}
     result = urlfetch.fetch(BLOG_FEED_URL, None, urlfetch.GET, fetch_headers)
@@ -32,7 +39,8 @@ class BlogpostsHandler(baseapp.BaseAppHandler):
       alllinks.append(bloglink)
       blog_data = {'title': item_title, 'link': item_link,
                    'pub_date': item_pub_date, 'description': item_description}
-      blogposts.Blogpost.create(blog_data)
+      post = blogposts.Blogpost()
+      post.create(blog_data)
 
     links = alllinks[:POST_COUNT]
     for link in links:
@@ -40,13 +48,10 @@ class BlogpostsHandler(baseapp.BaseAppHandler):
 
   @staticmethod
   def posts_for_display():
+    """ used by page handlers to get posts for display """
     posts = blogposts.Blogpost.get_newest_posts()
     return posts
 
-  @staticmethod
-  def get_newest_post():
-    posts = blogposts.Blogpost.description()
-    return posts[:1]
 
 urls = [
   ('/blog/update', BlogpostsHandler),
