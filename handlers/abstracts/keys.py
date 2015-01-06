57335c5fa50fc4dd3aee2bdbb9646b050d9448fb
Steven Young <fivepockets@gmail.com>
2015-01-05 22:30:24 -0800

    sample keys module

---
+++ handlers/abstracts/keys.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# get your own keys. put them here.
+
+tw_keys = dict()
+tw_keys['CONSUMER_KEY'] = ''
+tw_keys['CONSUMER_SECRET'] = ''
+tw_keys['OAUTH_TOKEN'] = ''
+tw_keys['OAUTH_TOKEN_SECRET'] = ''
+
+# https://www.goodreads.com/api/keys
+goodreads_keys = dict()
+
+gmaps_api_key = ''
